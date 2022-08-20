ecacd6a1cee868e61ced3eac5ce4735bc12089f2
MM <mm@kamiya.xrn.local>
2022-08-20 01:09:56 -0400

    more cleanup

--- rsync-mthehorrible.sh
+++
@@ -1,2 +0,0 @@
-# Hugo Github
-rsync -e "/usr/bin/ssh"  --bwlimit=2000 -av --delete /home/mm/apps/git/mthehorrible.cc/current/public/ mthehorrible@baratheon.dreamhost.com:/home/mthehorrible/mthehorrible.cc
