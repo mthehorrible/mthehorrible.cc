b38f275f1dbdebd33f63333c9f25dc295e119cbe
MM <mm@kamiya.xrn.local>
2022-08-20 01:00:17 -0400

    cleanup

--- rsync-mthehorrible.sh
+++ rsync-mthehorrible.sh
@@ -1,8 +1,2 @@
-# Jekyll
-#rsync -e "/usr/bin/ssh"  --bwlimit=2000 -av --delete /home/mm/apps/git/mthehorrible.cc/_site/ mthehorrible@baratheon.dreamhost.com:/home/mthehorrible/mthehorrible.cc
-
-# Zola
-#rsync -e "/usr/bin/ssh"  --bwlimit=2000 -av --delete /home/mm/apps/git/zola-sites/mthehorrible/public/ mthehorrible@baratheon.dreamhost.com:/home/mthehorrible/mthehorrible.cc
-
-# Zola Github
+# Hugo Github
 rsync -e "/usr/bin/ssh"  --bwlimit=2000 -av --delete /home/mm/apps/git/mthehorrible.cc/current/public/ mthehorrible@baratheon.dreamhost.com:/home/mthehorrible/mthehorrible.cc
